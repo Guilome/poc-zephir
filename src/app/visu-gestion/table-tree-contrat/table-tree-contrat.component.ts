1f02e33d48d8d223d785b053d99e182b836a77f3
Guillaume <guillaume.gobert@gmail.com>
2018-05-30 14:36:45 +0200

    fix ident des taches

--- src/app/visu-gestion/table-tree-contrat/table-tree-contrat.component.ts
+++ src/app/visu-gestion/table-tree-contrat/table-tree-contrat.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Contrat} from '../../shared/domain/contrat';
 import { ContratService } from '../../shared/services/contrat.service';
-import { Tache } from '../../shared/domain/Tache';
+import { Tache, Nature } from '../../shared/domain/Tache';
+import { TacheService } from '../../shared/services/tache.service';
 
 @Component({
   selector: 'table-tree-contrat',
@@ -10,32 +11,44 @@ import { Tache } from '../../shared/domain/Tache';
 })
 export class TableTreeContratComponent implements OnInit {
 
-  visible: Boolean = false
-  folderClass = []
-  contrats: Contrat[]
+  indexSup
   taches: Tache[]
+  dossiers: Tache[]
+  dossierPieces = []
   idCurrentUser;
 
-  constructor(public contratService: ContratService) {
+  constructor(public contratService: ContratService, private tacheService: TacheService) {
     this.idCurrentUser = parseInt(localStorage.getItem('USER'));
-    this.contrats = this.contratService.getContratByIdUtilisateur(this.idCurrentUser).filter(contrat => contrat.listeTaches.length > 0) 
+    this.tacheService.listerTaches().subscribe(data => this.taches = data)
+    this.dossiers = this.taches.filter( d => d.nature === Nature.DOSSIER && d.idUtilisateur === this.idCurrentUser)
   }
 
   ngOnInit() {
-    this.contrats.forEach(contrat => {
-      this.folderClass.push({bool : false, id: contrat.ident})
-      contrat.listeTaches.forEach(tache => {
-        contrat.idContext = tache.context.ident        
-      });      
-    })
-  }
-
-  toggleChildren(contrat: any){   
-    this.taches = contrat.listeTaches        
-    this.folderClass.forEach(folder =>{
-      if(folder.id == contrat.ident) {
-        folder.bool = !folder.bool
+    console.log(this.dossiers.length);
+    
+    this.dossiers.forEach(dossier => {
+      var lesPieces  = []
+      this.taches.forEach(tache => {
+        if (tache.nature === Nature.PIECE && tache.idTacheMere === dossier.ident){
+          lesPieces.push(tache)
+        }
+      })
+      if (lesPieces.length == 0) {
+        this.indexSup = this.dossiers.indexOf(dossier)
+      }
+      else {
+        this.dossierPieces.push({bool:false, dossier: dossier.ident, pieces: lesPieces})   
       }
     })
+    this.dossiers.splice(this.indexSup, 1)
+  }
+
+  toggleChildren(idTacheMere: number){           
+    this.dossierPieces.forEach(dossierP =>{      
+      if(dossierP.dossier === idTacheMere) {
+        dossierP.bool = !dossierP.bool
+      }
+    })
+    
   }
 }
