ac5ff38908d1256a3d6a02b76b903e00a25ce913
sirocco4 <igacha664@gmail.com>
2018-06-12 12:20:59 +0200

    action service temporaire

--- src/app/visu-gestion/visu-gestion.component.ts
+++ src/app/visu-gestion/visu-gestion.component.ts
@@ -3,6 +3,7 @@ import {Chart} from 'chart.js';
 import {TitreService} from '../shared/services/titre.service';
 import {Router} from '@angular/router';
 import { TacheService } from '../shared/services/tache.service';
+import { ActionMetierService } from '../shared/services/action-metier.service';
 @Component({
   selector: 'app-visu-gestion',
   templateUrl: './visu-gestion.component.html',
@@ -13,12 +14,14 @@ export class VisuGestionComponent implements OnInit {
 
   constructor(private titreService: TitreService, 
               private route: Router,
-              private tacheservice: TacheService) { }
+              private tacheservice: TacheService,
+              private actionMetierService: ActionMetierService) { }
 
   ngOnInit() {
     this.titreService.updateTitre('Tableau de Bord');
     // vider la liste des pieces temporaire
-    this.tacheservice.removePiecesTemporaire();     
+    this.tacheservice.removePiecesTemporaire();
+    this.actionMetierService.supprimerActionMetierTemporaire();   
   }
 
   ifConnexion(): boolean {
